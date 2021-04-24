09322c755afe57ccec3d47a059a3280c55ceb6b8
gluestacks <t.camera.pa@gmail.com>
2021-04-23 20:44:20 -0700

    4/23/21: Tester interface update for scaling

--- tracker_int.py
+++ tracker_int.py
@@ -4,11 +4,16 @@ import sys
 import numpy as np
 import pigpio
 import time
+import sys
 from time import sleep
 from math import sin, cos, radians
 detector = MTCNN()
 
-cap = cv2.VideoCapture(0)
+video_feed = 50
+while video_feed != 1 and video_feed != 0:
+    video_feed = int(input("Video Input Choice (0 or 1): "))
+
+cap = cv2.VideoCapture(video_feed)
 cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
 
 clrs_6 = (0, 255, 255)
@@ -19,8 +24,23 @@ y_deg_face = 1700
 pi.set_servo_pulsewidth(13, x_deg_face)
 pi.set_servo_pulsewidth(26, y_deg_face)
 
-factor = 0.5
-detection_factor = 0.7
+detection_factor = 0
+factor = 0
+while float(detection_factor) < 0.2 or float(detection_factor) > 1:
+    detection_factor = input("Enter detecting factor: ")
+
+while float(factor) < 0.1 or float(factor) > 1:
+    factor = input("Enter tracking factor: ")
+    if factor == "back":
+        factor = 0
+        detection_factor = input("Enter detecting factor: ")
+    if factor == "q":
+        sys.exit()
+
+detection_factor = float(detection_factor)
+factor = float(factor)
+
+
 x_ = int(220*factor)
 y_ = int(170*factor)
 w_ = int(640*factor-2*(x_))
@@ -106,8 +126,8 @@ while True:
 ##                    deg_change_face_y = 0.4*y_diff_face
 ##                    y_deg_face += deg_change_face_y
 ##                    pi.set_servo_pulsewidth(26, y_deg_face)
-                if x != None and default == 1:
-                    tracker = None
+                #if x != None and default == 1:
+                #    tracker = None
                 tracker = OPENCV_OBJECT_TRACKERS["csrt"]()
                 tracker.init(frame, (x,y,w,h))
                 t1 = time.perf_counter()
@@ -150,6 +170,8 @@ while True:
     
     if cv2.waitKey(1) &0xFF == ord('q'):
         break
+        
+        
         
 cap.release()
 cv2.destroyAllWindows()
