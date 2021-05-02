8a24445e4cf3d2c6b0a9d1bb3cec929c421d3169
gluestacks <t.camera.pa@gmail.com>
2021-05-01 21:23:20 -0700

    push test new account

--- start_by_enter.py
+++ start_by_enter.py
@@ -1,4 +1,7 @@
 import os
+import RPi.GPIO as GPIO
+ledPin = 7
 while True:
     input("Press Enter")
     os.system('python3 /home/pi/tracking_camera/prototype_v1_final.py')
+    GPIO.output(ledPin, GPIO.HIGH)
